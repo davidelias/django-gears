fe8c7f9c001344c28b3d83f44cfebdf0c6a8d818
David Elias <david@david-elias.net>
2012-11-30 09:19:07 +0000

    Add support for http cache in serve view

--- django_gears/views.py
+++ django_gears/views.py
@@ -1,3 +1,4 @@
+import os
 import mimetypes
 import posixpath
 import urllib
@@ -5,7 +6,9 @@ import urllib
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib.staticfiles.views import serve as staticfiles_serve
-from django.http import HttpResponse
+from django.views.static import was_modified_since
+from django.http import HttpResponse, HttpResponseNotModified
+from django.utils.http import http_date
 
 from gears.assets import build_asset
 from gears.exceptions import FileNotFound
@@ -18,16 +21,31 @@ def serve(request, path, **kwargs):
         raise ImproperlyConfigured(
             "The gears view can only be used in debug mode or if the "
             "--insecure option of 'runserver' is used.")
+
+    mimetype, encoding = mimetypes.guess_type(path)
+    if not mimetype in environment.mimetypes.values():
+        return staticfiles_serve(request, path, **kwargs)
+
     normalized_path = posixpath.normpath(urllib.unquote(path)).lstrip('/')
+
     try:
         asset = build_asset(environment, normalized_path)
     except FileNotFound:
         return staticfiles_serve(request, path, **kwargs)
-    if request.GET.get('body'):
-        asset = asset.processed_source
-    mimetype, encoding = mimetypes.guess_type(normalized_path)
-    mimetype = mimetype or 'application/octet-stream'
-    response = HttpResponse(asset, mimetype=mimetype)
+
+    bundle = not request.GET.get('body')
+    source = asset.compressed_source if bundle else asset.processed_source
+
+    # Respect the If-Modified-Since header.
+    size = len(source)
+    modified_since = request.META.get('HTTP_IF_MODIFIED_SINCE')
+    if not was_modified_since(modified_since, asset.mtime, size):
+        return HttpResponseNotModified(mimetype=asset.attributes.mimetype)
+
+    response = HttpResponse(source, mimetype=asset.attributes.mimetype)
+    response['Last-Modified'] = http_date(asset.mtime)
+    response['Content-Length'] = size
     if encoding:
         response['Content-Encoding'] = encoding
+
     return response
